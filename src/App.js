4b17842e091807c3e6b49dc37d58a0ce9a45f1ff
HJ <khjlove03@gmail.com>
2024-03-05 09:53:04 +0900

    컴퍼넌트완료

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import React from"react";
+import {BrowserRouter,Routes,Route} from"react-router-dom";
+
+import HomeView from "./views/HomeView";
 
 const App = () =>{
-  return<div>App</div>;
+  return(
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomeView/>}/>
+      </Routes>
+    </BrowserRouter>
+  );
+  
 };
 
 export default App;
