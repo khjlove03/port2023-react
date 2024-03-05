c81c5a3a7acf3f7441726363c8c061b618266ac4
HJ <khjlove03@gmail.com>
2024-03-05 14:42:13 +0900

    헤더영역 완료

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,62 @@
-import React from "react";
+import React,{useState} from "react";
+
+const headerNav = [
+    {
+        title: "intro",
+        url: "#intro",
+    },
+    {
+        title: "skill",
+        url: "#skill",
+    },
+    {
+        title: "site",
+        url: "#site",
+    },
+    {
+        title: "portfolio",
+        url: "#portfolio",
+    },
+    {
+        title: "contact",
+        url: "#contact",
+    },
+];
 
 const Header = () => {
+    const[show, setShow]=useState(false);
+
+    const toggleMenu = ()=> {
+        setShow((prevShow) => !prevShow);
+    };
     return(
-        <div>Header</div>
+        <header id="header" role="banner">
+            <div className="header__inner">
+                <div className="header__logo">
+                    <a href="/">portfolio<em>reactjs</em></a>
+                </div>
+                <nav className={'header__nav ${show ? "show" : ""}'} role="navigation" aria-label="메인메뉴">
+                    <ul>
+                        {headerNav.map((nav,key) => (
+                            <li key={key}>
+                                <a href={nav.url}>{nav.title}</a>
+                            </li>
+                        ))}
+                    </ul>
+                </nav>
+                <div 
+                    className="header__nav__mobile"
+                    id="headerToggle"
+                    area-controls="primary-menu"
+                    area-expanded={show?"true":"false"}
+                    role="button"
+                    tabIndex="0"
+                    onClick={toggleMenu}
+                >
+                    <span></span>
+                </div>
+            </div>
+        </header>
     );
 };
 
